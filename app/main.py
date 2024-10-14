86d82ab2e5eded04dc4b0c9fd5f365c96aab6cf6
Kasper Nielsen <kasper.kn.nielsen@jppol.dk>
2024-10-14 22:06:51 +0200

    refactor and added better search

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from movies import top_movies
 from fastapi import FastAPI
-from pydantic import BaseModel
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
@@ -11,27 +10,20 @@ Instrumentator().instrument(app).expose(app)
 # List of movies
 movies = top_movies
 
-# Pydantic model for a movie
-class Movie(BaseModel):
-    title: str
-    year: int
-    imdb_rating: float
-    cast: list
-
 # Get all movies
-@app.get("/movies/", response_model=list[Movie])
+@app.get("/movies/")
 async def get_movies():
     return movies
 
 # Get a single movie by title
-@app.get("/movies/{title}", response_model=Movie)
+@app.get("/movies/{title}")
 async def get_movie(title):
     for movie in movies:
-        if movie["title"] == title:
+        if title.lower() in movie["title"].lower():
             return movie
     return {"error": "Movie not found"}
 
-# Health check'
+# Health check
 @app.get("/health")
 async def healtcheck():
     return {"message": "ok"}
